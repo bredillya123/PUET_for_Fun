69ff265f3694aea63834de25793a746d8a2dc065
bredillya123 <bredillya@gmail.com>
2020-08-12 23:43:14 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        slidesToScroll: 1, 
+        speed: 500,
+        easing: 'ease',
+        initialSlide: 0,
+    });
+    
+});
+
